0a4146d524c71243a2ee5164283418ab420f4db3
weiguili <gta-qic@gmail.com>
2013-12-06 11:02:32 +0800

    coding

--- app/service/EService.java
+++ app/service/EService.java
@@ -1,8 +1,16 @@
 package service;
 
+import org.elasticsearch.ElasticSearchException;
+import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
+import org.elasticsearch.action.admin.indices.exists.indices.IndicesExistsRequest;
 import org.elasticsearch.action.admin.indices.mapping.put.PutMappingRequest;
+import org.elasticsearch.action.bulk.BulkRequestBuilder;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.deletebyquery.DeleteByQueryResponse;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.Requests;
 import org.elasticsearch.client.transport.TransportClient;
@@ -12,8 +20,12 @@ import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.index.query.FilterBuilder;
+import org.elasticsearch.index.query.FilterBuilders;
+import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.node.Node;
 import org.elasticsearch.node.NodeBuilder;
+import play.Logger;
 
 import java.io.IOException;
 import java.util.Date;
@@ -22,7 +34,7 @@ import java.util.Map;
 
 
 /**
- * desc: 这里提供的事ElasticSearch的服务
+ * desc: 提供的ElasticSearch的服务
  * User: weiguili(li5220008@gmail.com)
  * Date: 13-10-30
  * Time: 下午4:54
@@ -33,35 +45,230 @@ public class EService {
     public final static Settings settings = ImmutableSettings.settingsBuilder()
             .put("action.auto_create_index",false)
             .put("index.mapper.dynamic",false)
-            .put("number_of_shards",5) //分片
+            .put("number_of_shards",10) //分片
             .put("number_of_replicas",3)//副本
             .build();
 
     //Transport客户端
     public final static Client client = new TransportClient(settings)
             .addTransportAddress(new InetSocketTransportAddress("192.168.98.250", 9300));
+    private static EService eService = null;
+    private EService() {}
+    public static EService getInstance(){
+        if(eService ==null){
+            eService = new EService();
+        }
+        return eService;
+    }
 
-    public Map<String,Object> map;
+    /**
+     * 索引库是否存在
+     * @param IndicesName 索引库名称
+     * @return
+     */
+    public boolean isIndicesExist(String IndicesName){
+        return client.admin().indices().exists( new IndicesExistsRequest(IndicesName)).actionGet().isExists();
+    }
 
-    //获取索引
-    public GetResponse getIndex(){
-        GetResponse response = client.prepareGet("twitter","tweet","1")
+    /**
+     * 创建一个空索引库 (索引库名不能重复、不能是大写字母)
+     * @param name
+     * @return
+     */
+    public CreateIndexResponse createBlankIndices(String name){
+        CreateIndexResponse response = null;
+        try {
+            client.admin().indices().prepareCreate(name)
+                    .execute()
+                    .actionGet();
+        } catch (Exception e) {
+            Logger.error("创建空索引库出错！");
+            e.printStackTrace();
+        }
+        return response;
+    }
+
+    /**
+     * 用map添加
+     * @param indexName
+     * @param type
+     * @param id
+     * @param map
+     * @return
+     */
+    public IndexResponse addIndex(String indexName, String type, String id, Map<String,Object> map){
+        IndexResponse response = null;
+        try{
+            client.prepareIndex(indexName,type,id)
+                    .setSource(map)
+                    .execute()
+                    .actionGet();
+        }catch (Exception e){
+            Logger.error("创建索引出错！");
+            e.printStackTrace();
+        }
+        return response;
+    }
+
+    /**
+     * 用json添加索引
+     * @param indexName
+     * @param type
+     * @param id
+     * @param json
+     * @return
+     */
+    public IndexResponse addIndex(String indexName, String type, String id, String json){
+        IndexResponse response = null;
+        try{
+            client.prepareIndex(indexName,type,id)
+                    .setSource(json)
+                    .execute()
+                    .actionGet();
+        }catch (Exception e){
+            Logger.error("创建索引出错！");
+            e.printStackTrace();
+        }
+        return response;
+    }
+
+    /**
+     * 用builder添加索引
+     * @param indexName
+     * @param type
+     * @param id
+     * @param builder
+     * @return
+     */
+    public IndexResponse addIndex(String indexName, String type, String id, XContentBuilder builder){
+        IndexResponse response = null;
+        try{
+            client.prepareIndex(indexName,type,id)
+                    .setSource(builder)
+                    .execute()
+                    .actionGet();
+        }catch (Exception e){
+            Logger.error("创建索引出错！");
+            e.printStackTrace();
+        }
+        return response;
+    }
+
+    /**
+     * 添加索引（自动生成ID）
+     * @param indexName
+     * @param type
+     * @return
+     */
+    public IndexResponse addIndex(String indexName, String type,Map<String,Object> map){
+        return addIndex(indexName,type,null,map);
+    }
+    /**
+     * 添加索引（自动生成ID）
+     * @param indexName
+     * @param type
+     * @return
+     */
+    public IndexResponse addIndex(String indexName, String type,String json){
+        return addIndex(indexName,type,null,json);
+    }
+    /**
+     * 添加索引（自动生成ID）
+     * @param indexName
+     * @param type
+     * @return
+     */
+    public IndexResponse addIndex(String indexName, String type,XContentBuilder builder){
+        return addIndex(indexName,type,null,builder);
+    }
+
+    /**
+     * 获取索引
+     * @param indexName
+     * @param type
+     * @param id
+     * @return
+     */
+    public GetResponse getIndex(String indexName, String type, String id){
+        GetResponse response = client.prepareGet(indexName,type,id)
                 .execute()
                 .actionGet();
         return response;
     }
 
-    public void setMap() {
-        Map<String, Object> map = new HashMap<String, Object>();
-        map.put("user","Kimchy");
-        map.put("postDate",new Date());
-        map.put("message","trying out Elastic Search");
-        this.map = map;
+
+    public PutMappingRequest putMapping(XContentBuilder mapping) throws Exception{
+        PutMappingRequest mappingRequest = Requests.putMappingRequest("twitter")
+                .type("twitter")
+                .source(mapping);
+        client.admin().indices().putMapping(mappingRequest).actionGet();
+        return mappingRequest;
     }
 
-    //创建索引
-    public IndexResponse createIndex() throws IOException{
-        IndexResponse response = client.prepareIndex("twitterds","tweet","1")
+    public DeleteResponse deleteIndex(String indexName, String type, String id){
+        DeleteResponse response = client.prepareDelete(indexName, type, id)
+                .execute()
+                .actionGet();
+        return response;
+    }
+
+    public DeleteByQueryResponse deleteIndexByQuery(QueryBuilder query){
+        return client.prepareDeleteByQuery("twitter").setQuery(query).execute().actionGet();
+    }
+
+    /**
+     * 批量操作
+     * @param bulkRequest
+     * @return
+     */
+    public BulkResponse bulkOperation(BulkRequestBuilder bulkRequest){
+        BulkResponse bulkResponse = bulkRequest.execute().actionGet();
+        if (bulkResponse.hasFailures()) {
+            // process failures by iterating through each bulk response item
+        }
+        return bulkResponse;
+    }
+
+    /**
+     * 查询
+     * @param qb
+     * @param fb
+     * @return
+     */
+    public SearchResponse serch(QueryBuilder qb,FilterBuilder fb){
+        SearchResponse response = null;
+        client.prepareSearch()
+                .setQuery(qb)
+                .setFilter(fb)
+                .execute()
+                .actionGet();
+        return response;
+    }
+
+    /**
+     * 查询器查询
+     * @param qb
+     * @return
+     */
+    public SearchResponse serch(QueryBuilder qb){
+        SearchResponse response = null;
+        client.prepareSearch()
+                .setQuery(qb)
+                .execute()
+                .actionGet();
+        return response;
+    }
+
+    /**
+     * 过滤器查询
+     * @param fb
+     * @return
+     */
+    public SearchResponse serch(FilterBuilder fb){
+        SearchResponse response = null;
+        client.prepareSearch()
+                .setFilter(fb)
+                .setFrom(0).setSize(4).setExplain(true)
                 .execute()
                 .actionGet();
         return response;
--- test/service/EServiceTest.java
+++ test/service/EServiceTest.java
@@ -1,42 +1,144 @@
 package service;
 
+import org.elasticsearch.action.admin.indices.mapping.put.PutMappingRequest;
+import org.elasticsearch.action.bulk.BulkRequestBuilder;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.deletebyquery.DeleteByQueryResponse;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexResponse;
-import org.junit.BeforeClass;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.index.query.FilterBuilder;
+import org.elasticsearch.index.query.FilterBuilders;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import threadtest.ThreadTest2;
 
-import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * desc:
  * User: weiguili(li5220008@gmail.com)
- * Date: 13-11-11
- * Time: 下午2:43
+ * Date: 13-11-18
+ * Time: 下午2:56
  */
 public class EServiceTest {
-
-    //public static EService eService = new EService();
-    @BeforeClass
-    public static void setUpBeforeClass() throws Exception {
-        //eService = new EService();
+    private EService eService = null;
+    @Before
+    public void setUp() throws Exception {
+           eService = EService.getInstance();
+    }
+    @Test
+    public void testGetIndex() throws Exception {
+        GetResponse response = eService.getIndex("twitter","","M7dergw1QA6WJFjCNICDJw");
+        assertEquals("twitter", response.getIndex());
     }
 
     @Test
-    public void testCreateIndex() throws IOException {
-        EService eService = new EService();
-        eService.setMap();
-        IndexResponse indexResponse = eService.createIndex();
-        System.out.println("test");
+    public void testCreateIndex() throws Exception {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("user","frees");
+        map.put("postDate","2013");
+        map.put("message","trying out Elastic Search");
+
+        String json = "{" +
+                "\"user\":\"kimchy\"," +
+                "\"postDate\":\"2013-01-30\"," +
+                "\"message\":\"trying out Elastic Search\"," +
+                "}";
+
+        XContentBuilder builder = XContentFactory.jsonBuilder()
+                .startObject()
+                .field("user", "kimchy")
+                .field("postDate", "2013")
+                .field("message", "trying out Elastic Search")
+                .endObject();
+
+        IndexResponse indexResponse = eService.addIndex("twitter","tweets","1",map);
     }
 
     @Test
-    public void testGetIndex(){
-        EService eService = new EService();
-        GetResponse response = eService.getIndex();
-        System.out.println("ok");
+    public void testCreateBlankIndices() throws Exception {//不能创建重复的索引库
+        eService.createBlankIndices("free");
+    }
+
+    @Test
+    public void testPutMapping() throws Exception{
+        XContentBuilder mapping = XContentFactory.jsonBuilder()
+                .startObject()
+                .startObject("productindex")
+                .startObject("properties")
+                .startObject("title").field("type", "string").field("store", "yes").endObject()
+                .startObject("description").field("type", "string").field("index", "not_analyzed").endObject()
+                .startObject("price").field("type", "double").endObject()
+                .startObject("onSale").field("type", "boolean").endObject()
+                .startObject("type").field("type", "integer").endObject()
+                .startObject("createDate").field("type", "date").endObject()
+                .endObject()
+                .endObject()
+                .endObject();
+        PutMappingRequest pmr = eService.putMapping(mapping);
+    }
+
+    @Test
+    public void deleteIndex(){
+        DeleteResponse response = eService.deleteIndex("twitter", "tweets", "1");
+    }
+
+    @Test
+    public void testDeleteIndexByQuery(){
+        QueryBuilder query = QueryBuilders.fieldQuery("name", "Free");
+        DeleteByQueryResponse dbr = eService.deleteIndexByQuery(query);
+    }
+    @Test
+    public void testIsIndicesExist(){
+         assertEquals(true, eService.isIndicesExist("twitter"));
+    }
+    @Test
+    public void testBulkOperation() throws Exception{
+        BulkRequestBuilder bulkRequest = EService.client.prepareBulk();
+
+// either use client#prepare, or use Requests# to directly build index/delete requests
+        bulkRequest.add(EService.client.prepareIndex("twitter", "tweet", "1")
+                .setSource(XContentFactory.jsonBuilder()
+                        .startObject()
+                        .field("user", "kimchy")
+                        .field("postDate", new Date())
+                        .field("message", "trying out Elastic Search")
+                        .endObject()
+                )
+        );
+
+        bulkRequest.add(EService.client.prepareIndex("twitter", "tweet", "2")
+                .setSource(XContentFactory.jsonBuilder()
+                        .startObject()
+                        .field("user", "kimchy")
+                        .field("postDate", new Date())
+                        .field("message", "another post")
+                        .endObject()
+                )
+        );
+        BulkResponse response = eService.bulkOperation(bulkRequest);
+    }
+
+    @Test
+    public void testSearch(){
+        FilterBuilder filter = FilterBuilders.termFilter("user","frees");
+        SearchResponse response = eService.serch(filter);
+        //System.out.println(response.getHits().getTotalHits());
     }
 
 
-
+    @After
+    public void tearDown() throws Exception {
+        EService.client.close();
+    }
 }
